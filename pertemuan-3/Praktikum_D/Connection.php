5cf289e5449dc390a891e071f1d702fc8c6e8708
kefvin <kefvinsap03@gmail.com>
2025-12-09 16:50:26 +0700

    Praktikum D: destructor dasar

---
+++ pertemuan-3/Praktikum_D/Connection.php
@@ -0,0 +1,15 @@
+<?php
+
+class Connection {
+    public function __construct() {
+        echo "Koneksi dibuka\n";
+    }
+
+    public function __destruct() {
+        echo "Koneksi ditutup\n";
+    }
+}
+
+$c = new Connection();
+unset($c);
+echo "Selesai\n";
