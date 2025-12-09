f4d39e819c855347cb19e4a733fdbd5fac68c505
kefvin <kefvinsap03@gmail.com>
2025-12-09 16:43:30 +0700

    Praktikum C: pseudo constructor overloading

---
+++ pertemuan-3/Praktikum_c/User.php
@@ -0,0 +1,15 @@
+<?php
+class User {
+    public function __construct(
+        public string $name = "Guest",
+        public int $age = 0
+    ) {}
+}
+
+$u1 = new User();
+$u2 = new User("Rani");
+$u3 = new User("Budi", 25);  
+
+echo $u1->name . "\n"; 
+echo $u2->name . "\n";
+echo $u3->name . " - " . $u3->age . "\n";
